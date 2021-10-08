17cdbbc4e1bdb6681e2578ffdd03c589d5d925bd
HeeteshSimon <heeteshsimon@gmail.com>
2021-10-08 23:05:31 +0530

    Working on Landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { Switch } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Route } from 'react-router-dom';
 // import IdleTimerContainer from './IdealTime';
+import {Landing} from './Components/Pages/Landing';
+import {Login} from './Components/Pages/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Landing}/>
+        <Route path="/login" exact component={Login}/>
       </Switch>
       </BrowserRouter>
     </>
