dbdccf611019329e1d96c28eed0ac07d1524eb25
HeeteshSimon <heeteshsimon@gmail.com>
2021-10-09 19:54:51 +0530

    Init Changes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Container,Carousel} from "react-bootstrap";
 import { ReactComponent as BGImg } from "../lo.svg";
 
 const Landing = () => {
     return (
         <Container fluid>
+            <Carousel>
+                <Carousel.Item interval={1000}>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=First slide&bg=373940"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={500}>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Second slide&bg=282c34"
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Third slide&bg=20232a"
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
             <div className="main mt-5">
                 <div className="row">
                     <div className="col-md-6 col-12 h100 d-flex flex-column justify-content-center align-items-center">
